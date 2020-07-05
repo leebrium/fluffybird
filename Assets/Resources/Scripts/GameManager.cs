226d353afed0c639091a5c9b39f56072ec13fe64
kevinlee_app <kevinlee.appdev@gmail.com>
2020-07-05 23:44:18 +0700

    Add Splash screen, Adjust pipe, Update bird design, gravity and velocity

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -87,7 +87,7 @@ public class GameManager : MonoBehaviour
 
     public void Replay(){
         gameOver = false;
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void ContinueGame() {
